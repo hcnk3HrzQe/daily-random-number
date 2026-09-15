91073a2343ff176caf84132296613e4bd51eb74d
hcnk3HrzQe <39117042+hcnk3HrzQe@users.noreply.github.com>
2026-09-15 08:14:46 +0800

    v1.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.daily.random"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.10"
+        versionCode = 12
+        versionName = "1.11"
     }
 
     signingConfigs {
