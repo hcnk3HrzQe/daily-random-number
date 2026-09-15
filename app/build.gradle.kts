2e22e505f66dbbd66a23c2c085c306e38dff6ad5
hcnk3HrzQe <39117042+hcnk3HrzQe@users.noreply.github.com>
2026-09-15 09:05:02 +0800

    v1.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.daily.random"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.15"
+        versionCode = 17
+        versionName = "1.16"
     }
 
     signingConfigs {
